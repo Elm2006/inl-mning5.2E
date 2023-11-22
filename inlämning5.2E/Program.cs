a3edc23b5f4431e2522a24a74023c52570fb99ff
Elm2006 <isel06002@utb.vaxjo.se>
2023-11-22 09:57:56 +0100

    Add project files.

---
+++ inlämning5.2E/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace inlämning5.__2E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3] { 1, 2,0 };
+            Console.WriteLine("Skriv ett tal");
+            tal[2] = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(tal[i]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
